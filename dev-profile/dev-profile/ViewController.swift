921295421157ee99092c4594c7c5fbca932320cf
Aaron Johal <aaronjohal@gmail.com>
2020-04-07 10:49:35 +0100

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Aaron Johal on 07/04/2020.
+//  Copyright © 2020 Aaron Johal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
